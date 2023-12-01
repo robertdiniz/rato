5b538ecd4b5dcc8819ed5ef713df8a0d4b28f026
Robert <dinizrobert2002@gmail.com>
2023-12-01 15:04:42 -0300

    dog

--- rato.py
+++ rato.py
@@ -29,7 +29,8 @@ rat = pygame.transform.scale(pygame.image.load(RAT_IMAGE), (BLOCK_SIZE, BLOCK_SI
 exit_img = pygame.transform.scale(pygame.image.load(EXIT_IMAGE), (BLOCK_SIZE, BLOCK_SIZE))
 
 # Definição do mapa
-MAP_FILE = 'mapa2.txt'
+MAP_FILE = 'mapa3.txt'
+
 
 # Leitura do mapa do arquivo
 def read_map(file_path):
@@ -45,7 +46,7 @@ rows = len(game_map)
 cols = len(game_map[0])
 SCREEN_SIZE = (cols * BLOCK_SIZE, rows * BLOCK_SIZE)
 screen = pygame.display.set_mode(SCREEN_SIZE)
-pygame.display.set_caption('Labirinto')
+pygame.display.set_caption('Toby caçando osso')
 
 # Função para desenhar o mapa com imagens
 def draw_map_with_images(map_array):
@@ -59,43 +60,39 @@ def draw_map_with_images(map_array):
             elif map_array[row][col] == '1':
                 screen.blit(wall, (x, y))
             elif map_array[row][col] == 'm':
-                screen.blit(rat, (x, y))
+                screen.blit(ground, (x, y))  # Desenha o chão no lugar do 'm'
             elif map_array[row][col] == 'e':
-                screen.blit(exit_img, (x, y))
+                screen.blit(ground, (x, y))  # Desenha o chão
+                screen.blit(exit_img, (x, y))  # Desenha o osso acima do chão
+
 
 # Função para verificar se uma posição é válida no mapa
 def is_valid_position(pos):
     row, col = pos
     return 0 <= row < rows and 0 <= col < cols and game_map[row][col] != '1'
 
-# Função para encontrar o caminho usando backtracking com prioridade nas direções
-def find_path_backtracking(current_pos, exit_pos, visited, last_direction=None):
-    row, col = current_pos
-
+# Função para encontrar o caminho usando busca em profundidade (Depth-First Search - DFS)
+def find_path_dfs(current_pos, exit_pos, visited):
     if current_pos == exit_pos:
         return [current_pos]
 
     visited.add(current_pos)
 
-    # Ordem de movimento: direita, esquerda, baixo e cima
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
     for direction in directions:
-        if last_direction is not None and direction == (-last_direction[0], -last_direction[1]):
-            continue
-
-        new_row, new_col = row + direction[0], col + direction[1]
+        new_row, new_col = current_pos[0] + direction[0], current_pos[1] + direction[1]
         new_pos = (new_row, new_col)
 
         if is_valid_position(new_pos) and new_pos not in visited:
-            path = find_path_backtracking(new_pos, exit_pos, visited.copy(), direction)
+            visited.add(new_pos)  # Adiciona a nova posição antes da exploração
+            pygame.time.delay(100)  # Pausa de 100 milissegundos para visualização
+            draw_map_with_images(game_map)  # Redesenha o mapa com as explorações
+            screen.blit(rat, (new_col * BLOCK_SIZE, new_row * BLOCK_SIZE))  # Desenha o dog na nova posição
+            pygame.display.flip()
+            path = find_path_dfs(new_pos, exit_pos, visited.copy())
             if path:
                 return [current_pos] + path
-
-    if (row, col) != (0, 0) and last_direction is not None:
-        opposite_direction = (-last_direction[0], -last_direction[1])
-        path_to_corner = find_path_backtracking((0, 0), exit_pos, visited.copy(), opposite_direction)
-        return [current_pos] + path_to_corner if path_to_corner else []
 
     return []
 
@@ -119,42 +116,17 @@ def main():
         pygame.quit()
         sys.exit()
 
-    clock = pygame.time.Clock()
 
     visited = set()
-    path_to_exit = find_path_backtracking(rat_pos, exit_pos, visited)
+    path_to_exit = find_path_dfs(rat_pos, exit_pos, visited)
 
-    while path_to_exit:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        # Atualize a posição do dog para o próximo passo no caminho
-        rat_pos = path_to_exit.pop(0)
-
-        # Adicione a posição atual à lista de visitados
-        visited.add(rat_pos)
-
-        # Desenhe o mapa com imagens
-        draw_map_with_images(game_map)
-
-        # Pinte a trilha percorrida de roxo
-        for pos in visited:
-            pygame.draw.rect(screen, PURPLE, (pos[1] * BLOCK_SIZE, pos[0] * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-
-        # Desenhe o dog e a saída
-        screen.blit(rat, (rat_pos[1] * BLOCK_SIZE, rat_pos[0] * BLOCK_SIZE))
-        screen.blit(exit_img, (exit_pos[1] * BLOCK_SIZE, exit_pos[0] * BLOCK_SIZE))
-
+    if path_to_exit:
+        # Quando o dog encontra a saída
+        font = pygame.font.Font(None, 24)
+        text = font.render('Toby encontrou a saída!', True, WHITE)
+        screen.blit(text, (50, 50))
         pygame.display.flip()
-        clock.tick(5)  # 5 FPS para melhor visualização
 
-    # Quando o dog chegar à saída
-    font = pygame.font.Font(None, 36)
-    text = font.render('Dog encontrou a saída! Pressione R para reiniciar ou Q para sair.', True, WHITE)
-    screen.blit(text, (50, 50))
-    pygame.display.flip()
 
     while True:
         for event in pygame.event.get():
